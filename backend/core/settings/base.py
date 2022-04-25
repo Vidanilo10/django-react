b31f839eb0c802642e37ef09d863a3bff1912dac
Victor Castaneda <vidanilo10@gmail.com>
2022-04-24 22:59:00 -0500

    Add headers and fetch function

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -34,12 +34,14 @@ LOCAL_APPS = [
 THIRD_APPS = [
     'rest_framework',
     'simple_history',
+    "corsheaders",
 ]
 
 INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -103,6 +105,16 @@ USE_I18N = True
 USE_TZ = True
 
 AUTH_USER_MODEL = 'users.User'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:300",
+    "http://localhost:8080",
+]
+
+CORS_ORIGIN_WHITELIST= [
+    "http://localhost:300",
+    "http://localhost:8080",
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
