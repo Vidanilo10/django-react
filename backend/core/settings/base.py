32b8021232578c40cbdcc3c5b0f20a82da4ebf73
Victor Castaneda <vidanilo10@gmail.com>
2022-05-21 04:38:22 -0500

    Token configuration

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -35,6 +35,7 @@ THIRD_APPS = [
     'rest_framework',
     'simple_history',
     "corsheaders",
+    'rest_framework_simplejwt',
 ]
 
 INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
@@ -125,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
